e571f43a05819b11b771678651e9746116dfa30f
Aritro Sen <aritro1011@gmail.com>
2025-02-25 15:08:03 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,31 +1,27 @@
 import google.generativeai as genai
 import os
+import streamlit as st
 
+# Securely retrieve API key from Streamlit secrets
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 
-GEMINI_API_KEY = 'GEMINI API KEY' #user is instructed to add their own API Key here
 genai.configure(api_key=GEMINI_API_KEY)
-
 
 model = genai.GenerativeModel('gemini-pro')
 
-
 def generate_answer(query, retrieved_chunks):
-
     context = "\n".join(retrieved_chunks)
-
 
     prompt = f"""
     Use the following context to answer the question:
     Context: {context}
 
     Question: {query}
-    Also answer the question in detail.If there is less information to answer the question, you can also search the internet for more information. But when generating answer, you should also include the source of the information. Also format the output in a manner that the information that is retrieved from the uploaded document is highlighted.
+    Also answer the question in detail. If there is less information to answer the question, you can also search the internet for more information. But when generating the answer, you should also include the source of the information. Also format the output in a manner that the information that is retrieved from the uploaded document is highlighted.
     """
-
 
     response = model.generate_content(prompt)
     return response.text
-
 
 if __name__ == "__main__":
     query = "What is AI?"
